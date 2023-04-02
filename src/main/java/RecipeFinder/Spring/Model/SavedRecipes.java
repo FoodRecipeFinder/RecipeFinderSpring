b46ef065786d7bb316e895f7a85deca49bd90f4d
harshalaardekar101@gmail.com <harshalaardekar101@gmail.com>
2023-04-02 15:54:38 +0530

    model classes

--- src/main/java/RecipeFinder/Spring/Model/SavedRecipes.java
+++ src/main/java/RecipeFinder/Spring/Model/SavedRecipes.java
@@ -1,9 +1,13 @@
 package RecipeFinder.Spring.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,13 +16,18 @@ import lombok.ToString;
 @Entity
 @Getter
 @Setter
-@ToString
+@ToString(includeFieldNames = true)
 public class SavedRecipes {
 	@Id
 	@SequenceGenerator(initialValue = 1000,allocationSize = 1, name = "sr_seq")
 	@GeneratedValue(generator = "sr_seq", strategy = GenerationType.AUTO)
 	private int id;
-	private int userId;
 	private int mealId;
+//	private int userId;
 		
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	@JsonIgnore
+	User user;
+	
 }
